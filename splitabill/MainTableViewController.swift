5e62a8b80d2a512370c51ee76f0e941d4b5bb41d
pnk <pnk@pnk.local>
2018-01-08 21:24:47 +0900

    削除モードの追加

--- splitabill/MainTableViewController.swift
+++ splitabill/MainTableViewController.swift
@@ -77,6 +77,8 @@ class MainTableViewController: UITableViewController {
         //navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
         
         //navigationItem.rightBarButtonItem = UIBarButtonItem(title: "結果", style: .plain, target: self, action: #selector(MainTableViewController.addTapped))
+        
+        navigationItem.leftBarButtonItem = editButtonItem
 
     }
 
@@ -196,26 +198,30 @@ class MainTableViewController: UITableViewController {
         return cell
     }
     
-
-    /*
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        tableView.isEditing = editing
+    }
+    
     // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
+            tableData.remove(at: indexPath.row)
+            detailData.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
